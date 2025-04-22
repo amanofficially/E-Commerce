1a578848027d05cf885b6f90c1c71ed1710f3e7f
Aman Patel <amanofficially02@gmail.com>
2025-04-22 09:35:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    if (form) {
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Form submitted! (Demo purpose only)");
+      });
+    }
+  });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    const userName = localStorage.getItem("userName");
+
+    if (isLoggedIn === "true") {
+        const profile = document.getElementById("user-profile");
+        profile.style.display = "block";
+        document.getElementById("user-name").textContent = userName;
+    }
+});
